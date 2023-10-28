075c88048d930eb156aeb6ca9e42d5b15d2c7503
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-10-28 13:03:01 +0000

    [save]231028

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
+  # get 'users/edit'
+  # get 'users/show'
+  # get 'books/index'
+  # get 'books/edit'
+  # get 'books/new'
+  # get 'books/show'
+  resources :books, only: [:new, :index, :show]
+  resources :users, only: [:edit, :show, :index]
+  
+  # get 'homes/top'
+  root to: "homes#top"
+
+  get 'homes/about' => "homes#about" ,as:  'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
